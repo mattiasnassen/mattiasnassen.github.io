8159e3723307f8e5b8540124962303784bb32de2
mattiasnassen <mattias.nassen@gmail.com>
2016-12-12 20:18:42 +0100

    Commited

--- js/code.js
+++ js/code.js
@@ -1,15 +1,24 @@
+var checkPoint = false
 function giveAnswer() {
     var cheatcode = document.getElementById("text").innerHTML;
 if (cheatcode.endsWith("Up Up Down Down Left Right Left Right B A")) {
-    document.getElementById("coords").innerHTML = "Trying to cheat? Not needed, you have solved the mystery and can find the cache at N59 33.123 E016 30.123";}
+    document.getElementById("results").innerHTML = "<img src = 'img/endscreen.jpg'/>";
+    document.getElementById("text").innerHTML = "";
+    checkPoint = true;
+    }
 }
 function buttonPressA() {
 document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + " A";
     giveAnswer();
 }
 function buttonPressB() {
-document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + " B";
+    if(checkPoint == false){
+    document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + " B";
     giveAnswer();
+      }
+    else{
+        document.getElementById("coords").innerHTML = "Congratulations, the cache can be found at the following coordinates: N59 33.ABC E016 30.DEF";
+        }
     }
 function buttonPressSelect() {
 document.getElementById("text").innerHTML = document.getElementById("text").innerHTML + " Select";
