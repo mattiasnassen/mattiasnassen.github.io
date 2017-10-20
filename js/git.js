03bd09a99afe76585e24a19fac55d7b4984c7fbf
mattiasnassen <mattias.nassen@gmail.com>
2017-10-20 16:52:06 +0200

    Adding slideshow

--- js/git.js
+++ js/git.js
@@ -17,7 +17,33 @@ function buttonPressB() {
     giveAnswer();
       }
     else{
-        document.getElementById("coords").innerHTML = "Congratulations, the cache can be found at the following coordinates: N59 33.ABC E016 30.DEF";
+        document.getElementById("coords").innerHTML = "Congratulations, the coordinates can be found if the images shown are interpreted correclty. Hint 10=0 etc.";
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("dot");
+          if (n > slides.length) {slideIndex = 1}    
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
+          }
+          for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          dots[slideIndex-1].className += " active";
+        }
         }
     }
 function buttonPressSelect() {
