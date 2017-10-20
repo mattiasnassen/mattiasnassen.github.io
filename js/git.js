eaa99af265c5147e63767684eafd21ab9416a7e6
mattiasnassen <mattias.nassen@gmail.com>
2017-10-20 17:05:00 +0200

    Commit

--- js/git.js
+++ js/git.js
@@ -18,32 +18,8 @@ function buttonPressB() {
       }
     else{
         document.getElementById("coords").innerHTML = "Congratulations, the coordinates can be found if the images shown are interpreted correclty. Hint 10=0 etc.";
-        var slideIndex = 1;
-        showSlides(slideIndex);
-
-        function plusSlides(n) {
-          showSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-          showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-          var i;
-          var slides = document.getElementsByClassName("mySlides");
-          var dots = document.getElementsByClassName("dot");
-          if (n > slides.length) {slideIndex = 1}    
-          if (n < 1) {slideIndex = slides.length}
-          for (i = 0; i < slides.length; i++) {
-              slides[i].style.display = "none";  
-          }
-          for (i = 0; i < dots.length; i++) {
-              dots[i].className = dots[i].className.replace(" active", "");
-          }
-          slides[slideIndex-1].style.display = "block";  
-          dots[slideIndex-1].className += " active";
-        }
+        /* add url to next step*/
+        window.open("http://mattiasnassen.github.io/mario.html");
         }
     }
 function buttonPressSelect() {
