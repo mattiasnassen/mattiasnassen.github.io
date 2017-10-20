d59be116f0d5f1a29178b1309da59324b0cd9dc8
mattiasnassen <mattias.nassen@gmail.com>
2017-10-20 17:14:41 +0200

    Update

--- js/git.js
+++ js/git.js
@@ -17,8 +17,8 @@ function buttonPressB() {
     giveAnswer();
       }
     else{
-        location.href = "http://mattiasnassen.github.io/mario.html";
         document.getElementById("coords").innerHTML = "Congratulations, the coordinates can be found if the images shown are interpreted correclty. Hint 10=0 etc.";
+        document.write("<p>Link: " + txt.link("http://mattiasnassen.github.io/mario.html") + "</p>");
         }
     }
 function buttonPressSelect() {
